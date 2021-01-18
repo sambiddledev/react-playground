1be5172fd070ec75bcb3e683643d9424a1702d68
mindcycledev <sam@mindcycle.dev>
2021-01-17 21:36:59 -0500

    test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import ReactDom from 'react-dom'
 
+function Greeting() {
+  return ( <
+    div >
+    <
+    h4 > Hello!This is what you 've got!</h4> <
+    /div>
+
+  )
+};
+
+ReactDom.render( < Greeting / > , document.getElementById('root'));
